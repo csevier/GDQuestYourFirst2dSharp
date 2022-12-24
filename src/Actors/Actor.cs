104d0ff1950e7722298e5c624f95efe58b10c5e1
csevier <sevierit@gmail.com>
2022-12-24 14:18:55 -0600

    walls and gravity, Lecture Adding Floors

--- src/Actors/Actor.cs
+++ src/Actors/Actor.cs
@@ -4,4 +4,15 @@ using System;
 public class Actor : KinematicBody2D
 {
 
+    [Export]
+    public float Gravity { get; set;} = 3500.0f;
+    [Export]
+    public Vector2 Speed { get; set; } = new Vector2(300, 1000);
+    private Vector2 _velocity = Vector2.Zero;
+    
+    public override void _PhysicsProcess(float delta)
+    {
+        _velocity.y += this.Gravity * delta;
+        _velocity = this.MoveAndSlide(_velocity);
+    }
 }
